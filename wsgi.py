7ec6c9ca346bded75c3038bda4af9062f6b186eb
abhijeet <abhijeetdtu@gmail.com>
2017-02-04 17:11:31 +0530

    Changes to WSGI and inclusion in Solution

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
+    virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
     execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
+except Exception:
     pass
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
@@ -14,6 +14,10 @@ except IOError:
 
 def application(environ, start_response):
 
+
+    script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+    rel_path = "Pokhi\\Pokhi\\Views\\Index.html"
+    abs_file_path = os.path.join(script_dir, rel_path)
     ctype = 'text/plain'
     if environ['PATH_INFO'] == '/health':
         response_body = "1"
@@ -23,7 +27,7 @@ def application(environ, start_response):
         response_body = '\n'.join(response_body)
     else:
         ctype = 'text/html'
-        response_body = open("/Pokhi/Pokhi/Views/Index.html").read()
+        response_body = open(abs_file_path).read()
 
     status = '200 OK'
     response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
