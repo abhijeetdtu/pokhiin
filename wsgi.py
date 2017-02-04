41708f693e6d00c76df4e1c06ed55a17a1a33c1a
abhijeet <abhijeetdtu@gmail.com>
2017-02-04 20:14:42 +0530

    Commit WSGI

--- wsgi.py
+++ wsgi.py
@@ -45,4 +45,5 @@ if __name__ == '__main__':
     Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     httpd = SocketServer.TCPServer(("", PORT), Handler)
     print("serving at port", PORT)
+
     httpd.serve_forever()
