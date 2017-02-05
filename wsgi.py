49069b6956256fd4374b8945d3b7f70a0f9242dd
abhijeet <abhijeetdtu@gmail.com>
2017-02-06 00:04:43 +0530

    random check

--- wsgi.py
+++ wsgi.py
@@ -19,8 +19,7 @@ if __name__ == '__main__':
     #PORT = 8025
     #Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     #httpd = SocketServer.TCPServer(("", PORT), Handler)
-    #print("serving at port", PORT)
-
+    #print("serving at port", PORT
     #httpd.serve_forever()
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
