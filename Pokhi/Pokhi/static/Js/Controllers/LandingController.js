f7f6df54666bcbf0b9214594816cae62443b7fd5
abhijeet <abhijeetdtu@gmail.com>
2017-02-05 11:52:33 +0530

    testing explict state go

--- Pokhi/Pokhi/static/Js/Controllers/LandingController.js
+++ Pokhi/Pokhi/static/Js/Controllers/LandingController.js
@@ -1,7 +1,7 @@
 ﻿app.controller("LandingController", ['$scope', '$state', function ($scope, $state) {
 
     console.log("landing");
-    //$state.go("home");
+    $state.go("home");
     $scope.GoHome = function () {
         console.log("going home");
         $state.go("home");
