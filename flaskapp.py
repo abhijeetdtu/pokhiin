1aa16256a99cbbbfa52ea41fad7b98a66496ce10
abhijeet <abhijeetdtu@gmail.com>
2017-02-05 18:40:08 +0530

    mongosss

--- flaskapp.py
+++ flaskapp.py
@@ -13,7 +13,7 @@ app.config['MONGO_DBNAME'] = 'python'
 app.config['MONGO_URI'] =  'mongodb://admin:DJ7FltP4ZWLY@localhost:27017/python'
 
 if('OPENSHIFT_MONGODB_DB_URL' in  os.environ):
-    app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL']
+    app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL'] + app.config['MONGO_DBNAME']
 
 app.config['MONGO_USERNAME'] = 'admin'
 app.config['MONGO_PASSWORD'] = 'DJ7FltP4ZWLY'
