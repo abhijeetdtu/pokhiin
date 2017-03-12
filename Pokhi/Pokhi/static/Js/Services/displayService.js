f5140c19e8ce455c1bebd0ab981fe7777ac12398
abhijeet <abhijeetdtu@gmail.com>
2017-03-12 14:07:18 +0530

    Adding all landing options to Base

--- Pokhi/Pokhi/static/Js/Services/displayService.js
+++ Pokhi/Pokhi/static/Js/Services/displayService.js
@@ -1,6 +1,15 @@
 ﻿app.factory("displayService", [function () {
 
     var showingLoader = false;
+    var navItems = {};
+    navItems["Home"] = {};
+    navItems["Home"]["Main"] = [['Research', 'glyphicon glyphicon-stats', 'Views/Partials/Research.html']
+                                , ['Music', 'glyphicon glyphicon-headphones', 'Views/Partials/Music.html']
+                                , ['Geek', 'glyphicon glyphicon-qrcode', 'Views/Partials/Geek.html']
+                                , ['Pong', 'glyphicon glyphicon-qrcode', 'Apps/Pong/index.html']]
+
+    navItems["Base"] = {};
+    navItems["Base"]["Main"] = [['Upload', 'glyphicon glyphicon-stats', 'Views/Partials/UploadFile.html']]
 
     return {
         showLoader: function () {
@@ -18,11 +27,7 @@
                 case 'Home':
                     switch (navId) {
                         case 'Main':
-                            return [['Research', 'glyphicon glyphicon-stats' , 'Views/Partials/Research.html']
-                                , ['Music', 'glyphicon glyphicon-headphones', 'Views/Partials/Music.html']
-                                , ['Geek', 'glyphicon glyphicon-qrcode', 'Views/Partials/Geek.html']
-                                , ['Pong', 'glyphicon glyphicon-qrcode', 'Apps/Pong/index.html']
-                            ]
+                            return navItems[viewName][navId];
                             break;
                     }
                     break;
@@ -30,8 +35,7 @@
                 case 'Base':
                     switch (navId) {
                         case 'Main':
-                            return [['Upload', 'glyphicon glyphicon-stats', 'Views/Partials/UploadFile.html']
-                            ]
+                            return navItems["Home"]["Main"].concat(navItems["Base"]["Main"])
                             break;
                     }
                     break;
