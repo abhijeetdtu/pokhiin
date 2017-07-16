36a394ea2020c1d1f5a329bb9142756dd529e746
abhijeet <abhijeetdtu@gmail.com>
2017-07-16 21:09:40 +0530

    Adding cron job for feed

---
+++ .openshift/cron/hourly/FeedJobRun.sh
@@ -0,0 +1,3 @@
+cd $OPENSHIFT_REPO_DIR
+
+python -m Pokhi.Pokhi.OPS.FeedCreateJob
--- Pokhi/Pokhi/OPS/FeedCreateJob.py
+++ Pokhi/Pokhi/OPS/FeedCreateJob.py
@@ -11,12 +11,15 @@ class FeedCreateJob():
                 db = RawDBAccess.GetMongoDB()
                 table = db.wikipediaFeed
                 print("Starting ")
+                Logger.Log("FeedJob" ,"Feed job starting")
+                Logger.Log("FeedJob" ,"Feed job preparing feed")
                 print("PrepareFeed....")
                 WikiScrapper.PrepareFeed(table)
+                Logger.Log("FeedJob" ,"Feed job processing feed")
                 print("Process Feed....")
                 chef = FeedChef()
                 chef.ProcessRecords()
-                print("Sleep....10 mins starting now.." +  str(time.time))
+                Logger.Log("FeedJob" ,"Feed job processing feed done")
             except Exception as e:
                 print(e)
                 Logger.Log("Exception :FeedCreate job" ,e.message)
